48d1b928557ad7c34cb976a79b34447999c51ece
JBravo <bravo.jose.luis18@gmail.com>
2020-08-01 22:06:30 -0300

    update angular 9, add nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule, NbSidebarService } from '@nebular/theme';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NbThemeModule.forRoot(),
+    RouterModule.forRoot([]),
+    NbLayoutModule,
+    NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
   ],
-  providers: [],
+  providers: [NbSidebarService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
